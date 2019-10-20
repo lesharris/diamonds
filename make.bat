fc623f29d23c81d4f4222092b255cec6a68307c9
Les Harris <les@cloudpwr.com>
2019-10-20 14:32:50 -0700

    Allow .bin extensions

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 del *.o
-del *.bin
+del *.obj
 del *.txt
 del *.nl
 del *.dbg
@@ -9,7 +9,7 @@ ca65 -o ram.o -g -D RAM_EXPORT ram.s
 ca65 -o header.o -g header.s
 ca65 -o diamonds.o -g diamonds.s
 
-ld65 -o header.bin -C .\header.cfg header.o
-ld65 -o diamonds.bin -C .\linker.cfg --dbgfile diamonds.dbg ram.o diamonds.o
+ld65 -o header.obj -C .\header.cfg header.o
+ld65 -o diamonds.obj -C .\linker.cfg --dbgfile diamonds.dbg ram.o diamonds.o
 
-copy /b header.bin+diamonds.bin diamonds.nes
+copy /b header.obj+diamonds.obj diamonds.nes
